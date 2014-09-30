d317ceb652565e69caafe5069bd650baac3c9f46
Nathan Osman <nathan.osman@gmail.com>
2014-09-29 18:37:52 -0700

    Corrected path to WSGI application.

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ src/manage.py migrate
 uwsgi \
   --http-socket 0.0.0.0:80 \
   --plugin python \
-  --chdir /root/src \
+  --chdir /data/src \
   --module twobuntu.wsgi \
   --static-map /media=/data/www/media \
   --static-map /static=/data/www/static
